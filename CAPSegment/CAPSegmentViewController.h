03f14d31ce1d89df2ed46e717a552effdca9ae42
captain9911 <BOBO@BOBOdeiMac.local>
2016-07-18 10:48:56 +0800

    部分优化

--- CAPSegment/CAPSegmentViewController.h
+++ CAPSegment/CAPSegmentViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by captain9911 on 16/6/22.
 //  Copyright © 2016年 captain9911. All rights reserved.
-//
+//  https://github.com/captain9911/CAPSegment.git
 
 #import <UIKit/UIKit.h>
 
